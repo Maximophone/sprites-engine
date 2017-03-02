291858e04410166baaf072729f09f24dcd79c27b
Maxime Fournes <mf@seldon.io>
2017-03-02 19:24:17 +0000

    Started turning slime into finite state machine. Reduced size of map for testing

--- entities/Slime.py
+++ entities/Slime.py
@@ -1,18 +1,35 @@
 import random
 from engine import Entity
 
+class State(object):
+    def __init__(self,entity):
+        self.entity = entity
+        
+    def get_dirs(self):
+        _,dirs = self.entity.ground_map.get_val_and_direct_neighbours(self.entity.x,self.entity.y)
+        possible_dirs = [i for i,x in enumerate(dirs) if x==1]
+        if not possible_dirs:
+            possible_dirs = [4]
+        return possible_dirs
+        
+class S_EXPLORE(State):
+    def update(self):
+        possible_dirs = self.get_dirs()
+        d = random.choice(possible_dirs)        
+        self.entity.move(d)
+        
+class S_COMEBACK(State):
+    pass
+    
+
 class Slime(Entity):
     clazz = 'slime'
 
     def init(self,ground_map):
         self.ground_map = ground_map
-
+        self.state = S_EXPLORE(self)
+        
     def update(self):
-        _,dirs = self.ground_map.get_val_and_direct_neighbours(self.x,self.y)
-        possible_dirs = [i for i,x in enumerate(dirs) if x==1]
-        if not possible_dirs:
-            possible_dirs = [4]
-        d = random.choice(possible_dirs)
-        self.move(d)
-
-        neighbours = self.get_neighbours()
+        new_state = self.state.update()
+        if new_state is not None:
+            self.state = new_state
